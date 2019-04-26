e918482cf5b71581c6f0ce980e85043172d3ed80
yuji-hatakeyama <htkymyj0512@gmail.com>
2019-04-26 21:04:19 +0900

    Initial commit.

---
+++ src/app.js
@@ -0,0 +1,16 @@
+// example1
+{
+    console.log("Hello! Node.js × TypeScript");
+}
+// example2
+{
+    let message = '';
+    message = "TypeScript World";
+    //message = 2;
+    console.log("Hello!" + message);
+}
+// example3
+{
+    let message = '';
+}
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,17 @@
+// example1
+{
+    console.log("Hello! Node.js × TypeScript");
+}
+
+// example2
+{
+    let message : String = '';
+    message = "TypeScript World"
+    //message = 2;
+    console.log("Hello!" + message);
+}
+
+// example3
+{
+    let message : String = '';
+}
